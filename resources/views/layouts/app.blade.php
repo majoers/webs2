66018fcc6fc36f4ec7da46a8644efdee3453f0e9
Roel Rijken <rjwrijke@avans.nl>
2018-03-30 21:28:06 +0200

    Add icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="{{ asset('css/product.css')}}" rel="stylesheet" >
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 
 </head>
 <body>
